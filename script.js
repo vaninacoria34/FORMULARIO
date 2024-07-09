127fcadfd683af3cf8b2444de646ddd8c81cb804
vaninacoria34 <vaninamariselcoria75@gmail.com>
2024-07-08 21:18:05 -0300

    FORMULARIO

---
+++ script.js
@@ -0,0 +1,8 @@
+function switchStyle() {
+    var currentStyle = document.querySelector('link[rel=stylesheet]');
+    if (currentStyle.getAttribute('href') === 'style.css') {
+        currentStyle.setAttribute('href', 'high-contrast.css');
+    } else {
+        currentStyle.setAttribute('href', 'style.css');
+    }
+}
