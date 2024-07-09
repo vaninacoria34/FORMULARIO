aa4d5fa119b36741056fa6749c03804bb6409fbb
vaninacoria34 <vaninamariselcoria75@gmail.com>
2024-07-09 18:03:06 -0300

    FORMULARIO

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function switchStyle() {
     var currentStyle = document.querySelector('link[rel=stylesheet]');
     if (currentStyle.getAttribute('href') === 'style.css') {
-        currentStyle.setAttribute('href', 'high-contrast.css');
+        currentStyle.setAttribute ('href', 'high-contrast.css');
     } else {
         currentStyle.setAttribute('href', 'style.css');
     }
